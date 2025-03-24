0fe202415fda5fb158203f62f4453bfcd0205c19
Olga Popova <vickyg@yandex.ru>
2025-03-24 15:56:06 +0300

    Correction according to flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = os.environ.get('DEBUG') == True
+DEBUG = os.environ.get('DEBUG') is True
 
 ALLOWED_HOSTS = ['infrasprint1.site', '158.160.82.101']
 
